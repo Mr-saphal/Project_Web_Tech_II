551f6a2dce40ebe1518ffe176081872837f72856
Mr-saphal <saphaldhamala10@gmail.com>
2026-08-16 20:23:31 +0545

    changes in header

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chatta-Fatta | Food Ordering System</title>
+</head>
+<body>
+  <nav>
+    <div>
+      <a href="index.php">Chatta-Fatta</a>
+
+      <div>
+        <ul>
+       <a href="menu.php">Menu</a></li>
+        <a href="myorders.php">My Orders</a></li>
+        <a href="cart.php">Cart</a></li>
+        <a href="user-login.php">Login</a></li>
+        <a href="myprofile.php">My Profile</a></li>
+        <a href="register.php">Sign Up</a></li>
+
+        </ul>
+    </div>
+    </div>
+
+  
+</body>
+</html>
